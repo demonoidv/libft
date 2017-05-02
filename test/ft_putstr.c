f32167c89d3f5b617615b3bafc626d7baa14b715
Valentin SPORER <vsporer@e2r10p14.42.fr>
2017-05-02 11:09:44 +0200

    remove useless files

--- test/ft_putstr.c
+++
@@ -1,7 +0,0 @@
-#include "libft.h"
-
-void	ft_putstr(char const *s)
-{
-	if (s)
-		write(1, s, ft_strlen(s));
-}
--- test/ft_putstr_fd.c
+++
@@ -1,7 +0,0 @@
-#include "libft.h"
-
-void	ft_putstr_fd(char const *s, int fd)
-{
-	if (s)
-		write(fd, s, ft_strlen(s));
-}
