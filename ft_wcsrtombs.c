b710bc16bfaf414ec652f580f52a981c40dd38dd
DemonoidV <demodev@Mac-Pro-de-DemonoidV.local>
2017-07-17 17:32:02 +0200

    modif ft_wcsrtombs.c

--- ft_wcsrtombs.c
+++ ft_wcsrtombs.c
@@ -3,9 +3,9 @@
 size_t	ft_wcsrtombs(char *str, wchar_t *wcs, size_t lenmax)
 {
 	int		i;
-	int		size;
 	int		ret;
 	size_t	res;
+	size_t	size;
 
 	i = 0;
 	ret = 0;
@@ -14,9 +14,9 @@ size_t	ft_wcsrtombs(char *str, wchar_t *wcs, size_t lenmax)
 		return (0);
 	if (!str)
 		return (size);
-	while (ret < size)
+	while (res < size)
 	{
-		ret = ft_wcrtomb((str + ret), wcs[i]);
+		ret = ft_wcrtomb(&(str[res]), wcs[i]);
 		res += ret;
 		i++;
 	}
