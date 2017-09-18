b25d5b5c7fff30f9ae7ac74671367b35bad71fb5
Valentin SPORER <vsporer@e2r12p9.42.fr>
2017-09-18 20:43:10 +0200

    add header

--- src/ft_printf/ft_printf.c
+++ src/ft_printf/ft_printf.c
@@ -6,56 +6,11 @@
 /*   By: vsporer <vsporer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/09 15:58:49 by vsporer           #+#    #+#             */
-/*   Updated: 2017/09/05 19:08:16 by demodev          ###   ########.fr       */
+/*   Updated: 2017/09/18 12:02:22 by vsporer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-/*
-static int	ft_printf_tostr(char **ret, t_list *list)
-{
-	t_list	*todel;
-	char	*tmp;
-	int		size;
-	int		i;
-
-	todel = list;
-	size = 1;
-	i = 0;
-	while (todel && (size += todel->content_size))
-		todel = todel->next;
-	*ret = ft_strnew((size_t)size);
-	tmp = *ret;
-	while (list && *ret)
-	{
-		todel = list;
-		ft_memcpy(tmp, list->content, (list->content_size));
-		tmp = ft_strchr(tmp, '\0');
-		i += list->content_size;
-		ft_memdel(&(list->content));
-		list = list->next;
-		free(todel);
-	}
-	return (i);
-}
-
-int			ft_asprintf(char **ret, const char *format, ...)
-{
-	va_list	ap;
-	t_list	*list;
-	int		i;
-	int		nullchar;
-
-	i = 0;
-	nullchar = 0;
-	va_start(ap, format);
-	list = ft_printf_tolst((char*)format, &nullchar, &ap);
-	va_end(ap);
-	if (list)
-		i = ft_printf_tostr(ret, list);
-	return (i);
-}
-*/
 
 static int	print_format(char *format)
 {
@@ -93,8 +48,7 @@ int			ft_printf(const char *format, ...)
 		}
 		else if (*format == '%')
 			format++;
-		i = print_format((char*)format);
-		ret += i;
+		ret += (i = print_format((char*)format));
 	}
 	return (ret);
 }
